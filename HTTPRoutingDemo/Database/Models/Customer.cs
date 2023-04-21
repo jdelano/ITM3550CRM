9194d4121d457ba50af69a4f1cb4d6613669d9e6
John Delano <jdelano@cedarville.edu>
2023-04-21 12:41:42 -0400

    Fixed default values for Customer class.

--- HTTPRoutingDemo/Database/Models/Customer.cs
+++ HTTPRoutingDemo/Database/Models/Customer.cs
@@ -4,8 +4,8 @@ namespace HTTPRoutingDemo.Database.Models
     public class Customer
     {
         public int CustomerId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
         public double Balance { get; set; }
     }
 }
